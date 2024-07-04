7d2977eacfaca498ba93d63c11e0e64ef1aa672a
Ricardo Santos <santosqa@mac.local>
2024-07-04 20:38:39 -0300

    first commit

---
+++ HELP.md
@@ -0,0 +1,12 @@
+/** 
+
+  Anotações
+  
+  - Corrigir textos dos links de assessibilidade
+  - Criar projeto linkbio apartamento vista mar como template de venda
+  - Criar post no site santos qa 
+  - Criar post na comunidade AIRBNB
+  - Incluir botão no link bio com o link do artigo 
+  - verificar vulnerabilidades e assessibilidade
+
+**/
---
+++ script.js
@@ -0,0 +1,84 @@
+function toggleMode() {
+  const html = document.documentElement;
+  document.documentElement.classList.toggle('light');
+}
+
+document.addEventListener('DOMContentLoaded', function () {
+  const whatsappShareButton = document.getElementById('whatsapp-share');
+  whatsappShareButton.addEventListener('click', handleWhatsappShareClick);
+
+  document.getElementById('share-button').addEventListener('click', handleShareButtonClick);
+
+  document.querySelector('.close-button').addEventListener('click', closeShareModal);
+  window.addEventListener('click', handleWindowClick);
+
+  const shareLinks = document.querySelectorAll('#fallback-share-options a');
+  shareLinks.forEach(link => {
+    link.addEventListener('click', closeShareModal);
+  });
+});
+
+function handleWhatsappShareClick(  ) {
+  event.preventDefault();
+  const currentUrl = encodeURIComponent(window.location.href);
+  const shareText = encodeURIComponent('Captei uma URL de potencial épico no vasto éter digital e imediatamente associei à sua persona. 🌐 Nela há hyperlinks que, segundo os padrões do universo cibernético, podem ser muito úteis. Recomendo uma inspeção, pois a jornada promete recompensas! 🚀');
+  const whatsappUrl = `https://api.whatsapp.com/send?text=${shareText}%20${currentUrl}`;
+  window.open(whatsappUrl, '_blank');
+}
+
+function handleShareButtonClick(event) {
+  event.preventDefault();
+
+  const shareData = {
+    title: document.title,
+    text: 'Captei uma URL de potencial épico no vasto éter digital e imediatamente associei à sua persona. 🌐 Nela há hyperlinks que, segundo os padrões do universo cibernético, podem ser muito úteis. Recomendo uma inspeção, pois a jornada promete recompensas! 🚀',
+    url: window.location.href,
+  };
+
+  if (navigator.share) {
+    shareViaWebShareApi(shareData);
+  } else {
+    shareViaFallbackMethods(shareData);
+  }
+}
+
+function shareViaWebShareApi(shareData) {
+  navigator
+    .share(shareData)
+    .then(handleShareSuccess)
+    .catch(handleShareError);
+}
+
+function shareViaFallbackMethods(shareData) {
+  const shareUrl = encodeURIComponent(shareData.url);
+  const shareTitle = encodeURIComponent(shareData.title);
+  const shareText = encodeURIComponent(shareData.text);
+
+  document.getElementById('linkedin-share').href = `https://www.linkedin.com/sharing/share-offsite/?text=${shareText}%0A${shareUrl}%0A`;
+  document.getElementById('whatsapp-share2').href = `https://api.whatsapp.com/send?text=${shareText}%0A${shareUrl}%0A`;
+  document.getElementById('twitter-share').href = `https://twitter.com/intent/tweet?text=${shareText}%0A&url=${shareUrl}%0A`;
+  document.getElementById('facebook-share').href = `https://www.facebook.com/sharer/sharer.php?u=${shareUrl}`;
+  document.getElementById('email-share').href = `mailto:?subject=${shareTitle}&body=${shareText}%0A${shareUrl}%0A`;
+  document.getElementById('telegram-share').href = `https://t.me/share/url?url=${shareUrl}%0A&text=${shareText}`;
+  document.getElementById('instagram-share').href = `https://www.instagram.com/santosqa_`;
+  document.getElementById('share-modal').style.display = 'block';
+}
+
+  function handleShareSuccess() {
+  console.log('Página compartilhada com sucesso!');
+}
+
+function handleShareError(error) {
+  console.error('Erro ao compartilhar', error);
+}
+
+function closeShareModal() {
+  document.getElementById('share-modal').style.display = 'none';
+}
+
+function handleWindowClick(event) {
+  if (event.target == document.getElementById('share-modal')) {
+    closeShareModal();
+  }
+}
+
